a0e0bac0032abf985f2caadebdec6fbe554af73e
Kobe <irissneo@gmail.com>
2026-04-17 00:06:16 +0200

    finally setup db remotely and changed all connection details

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "devuser",
-    host: "localhost",
-    database: "devdb",
-    password: "devpass",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false },
 });
 
 pool.connect((err, client,release) => {
