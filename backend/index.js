8e4362a3e8ad202578894830836b83ba8768dda4
Kobe <irissneo@gmail.com>
2026-06-02 17:42:13 +0200

    adjust the ordering in the index file for keeping server awake and allowing other api requests

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,12 @@ const apiRouter = require("./api");
 
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
-app.use("/api", apiRouter);
+
 app.get("/health", (req, res) => {
-  res.json({ success: true });
+  res.json({ success: true, message: "Server awake" });
 });
+
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
