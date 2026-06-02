8b8d710d496c04c435a08a683bee1ae15a1a30da
Kobe <irissneo@gmail.com>
2026-06-02 16:44:23 +0200

    import sendexpopush correctly in marketplace endpoint

--- backend/gateCodes.js
+++ backend/gateCodes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("./db");
+const { sendExpoPush } = require("./services/pushService");
 const authenticateToken = require("./middleware/auth");
 const {
   generateAndAssignGateCodes,
