3672fb9cd5604834095ee0a23f7e060af0b289e3
Kobe <irissneo@gmail.com>
2026-06-27 15:39:13 +0200

    add cors in server file to allow localhost ip address for dev changnes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
+
 // index.js
 require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const apiRouter = require("./api");
-
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: [
+    "http://localhost:8081",
+    "http://10.162.147.147:8081",
+    "https://community-app-ccmd.onrender.com",
+  ],
+  credentials: true,
+}));
 
 app.use(express.json());
 
